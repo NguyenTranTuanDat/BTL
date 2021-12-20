119899cdf1722d05600a6fc721c05e9d24aa70ef
NguyenTranTuanDat <tuandat099@gmail.com>
2021-12-20 12:41:11 +0700

    them link

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <header class="component">
             <a href="index.html"><img src="./images/logo.svg" class="logo" /></a>
             <nav>
-                <li><img src="./images/news.svg" /> Bảng tin</li>
+                <a href="#"><li><img src="./images/news.svg" /> Bảng tin</li></a>
                 <li>
                     <img src="./images/experience.svg" class="icon" /> Trải
                     nghiệm
