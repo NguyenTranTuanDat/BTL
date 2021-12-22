7f51978787b650e4ad86d141223b48295af6fc58
NguyenTranTuanDat <tuandat099@gmail.com>
2021-12-22 16:43:49 +0700

    search and before

--- index2.php
+++ index2.php
@@ -5,18 +5,24 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="./css/reset.css" />
-        <link rel="stylesheet" href="./css/header.css" />
+        <link rel="stylesheet" href="./css/header2.css" />
         <link rel="stylesheet" href="./css/left-section.css" />
         <link rel="stylesheet" href="./css/right-section.css" />
         <link rel="stylesheet" href="./css/main.css" />
         <title>Bảng tin | Hahalolo</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
 
     <body>
         <!-- Header -->
         <header class="component">
-            <a href="index.php"><img src="./images/logo.svg" class="logo" /></a>
+                <a href="index2.php"><img src="./images/logo.svg" class="logo" /></a>
+                <form class="search">
+                    <i class="bi bi-search"></i>
+                    </i><input type="text" name="search" placeholder="Search..">
+                    <i class="bi bi-chevron-down"></i>
+                </form>
             <nav>
                 <li><img src="./images/news.svg" /> Bảng tin</li>
                 <li>
